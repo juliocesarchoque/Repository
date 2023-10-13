ff645066be2d498d43c51f1f5e8a95c9cb2bd283
juliocesarchoque <115742217+juliocesarchoque@users.noreply.github.com>
2023-10-13 17:00:05 -0400

    Repeat-Script
    this is a script in python for repeat mensages

---
+++ Script-repeat.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+# Get the message you want to repeat
+message = input("Mensaje prueba ")
+# Get the number of times you want to repeat the message
+num_repeats = 5
+# Repeat the message the specified number of times
+for i in range(num_repeats):
+    pyautogui.typewrite(message)
+    pyautogui.press("enter")
+    time.sleep(1)
