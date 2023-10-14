7f1b81205f0c3064fa1cf64d98c1c3ba11dfa52c
juliocesarchoque <115742217+juliocesarchoque@users.noreply.github.com>
2023-10-14 10:50:02 -0400

    Update Script-repeat.py

--- Script-repeat.py
+++ Script-repeat.py
@@ -1,13 +1,9 @@
 import pyautogui
 import time
-# Get the message you want to repeat
-message = input("Mensaje prueba ")
 # Get the number of times you want to repeat the message
 num_repeats = 5
-# Repeat the message the specified number of times
+
+time.sleep(3)
 for i in range(num_repeats):
-    pyautogui.typewrite(message)
+    pyautogui.write("hola")
     pyautogui.press("enter")
-    time.sleep(1)
-#install this:
-    pip install pyautogui
